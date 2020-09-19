c2cd6efdf4fa4b1d8f96a2d5f4a42cd313a0cade
Lucas Vilela <lrvilelab@hotmail.com>
2020-09-19 01:37:50 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieDta = { // não haver quebra do componente gráfico
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-lucas.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
